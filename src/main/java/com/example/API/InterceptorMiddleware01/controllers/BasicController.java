77669245824b50cb703d70dfede6bd9ad25a93e1
Ana-Maria B <new_annemarie@yahoo.com>
2024-03-16 01:18:40 +0100

    Update BasicController.java

--- src/main/java/com/example/API/InterceptorMiddleware01/controllers/BasicController.java
+++ src/main/java/com/example/API/InterceptorMiddleware01/controllers/BasicController.java
@@ -14,5 +14,5 @@ public class BasicController {
     public String getCurrentDateTime() {
         return  "Current date and time: " + LocalDateTime.now();
     }
-    //http://localhost:8080/time
+    //localhost:8080/time
 }
